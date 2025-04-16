42e60d4330e7e6441f2a3e7a956e15daeb6af9e6
minjoon1015 <3283alswns@naver.com>
2025-04-16 14:32:15 +0900

    first commit

---
+++ back-end/src/main/java/FutureCraft/tikitaka/back_end/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,11 @@
+package FutureCraft.tikitaka.back_end.filter;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter {
+    
+}
---
+++ back-end/src/main/java/FutureCraft/tikitaka/back_end/provider/jwtProvider.java
@@ -0,0 +1,55 @@
+package FutureCraft.tikitaka.back_end.provider;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+
+@Component
+public class jwtProvider {
+    @Value("${secret-key}")
+    private String secretKey;
+
+    // token 생성
+    public String create(String id) {
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        String jwt = Jwts.builder()
+                    .signWith(key, SignatureAlgorithm.HS256)
+                    .setSubject(id)
+                    .setIssuedAt(new Date())
+                    .setExpiration(expiredDate)
+                    .compact();
+        return jwt;
+    }
+    
+    // token 해석
+    public String validate(String jwt) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        Claims claims = null;
+
+        try {
+            claims = Jwts
+                    .parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwt)
+                    .getBody();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+       }
+       return claims.getSubject();
+    }
+}
